90042b6ba4b91fb1ce2c00b7cee76958acb363bd
izphilp <philbaloa2@gmail.com>
2023-10-20 18:39:24 -0600

    writingDone

--- 1.py
+++ 1.py
@@ -1,24 +1,24 @@
-
+count = 0
+count1 = 0 
+count2= 0
 file_open = open('sample.ini','r')
 data = file_open.read()
 
 for vowels in data:
     if vowels in ["a","e","i","o","u"]:
-        count = 0
         count += 1
-        print(f"vowel count {count}")
+        c =(f" vowel count {count}")
 
 for letter in data:
-    count1 = 0
     count1 += 1
-    print(f"letter count {count1}")
+    b =(f" letter count {count1}")
 
 for num in data:
     if num.isdigit():
-        count2 = 0
         count2 += 1
-        print(f"number count {count2}")
+        a =(f" number count {count2}")
+d = a + b + c
+print(d)
 
-
-#with open("counts.txt","w") as fileWriter:
-   # fileWriter.write(str(a))
+with open("counts.txt","w") as fileWriter:
+    fileWriter.write(d)
